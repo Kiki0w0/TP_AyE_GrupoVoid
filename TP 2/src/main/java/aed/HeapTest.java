78216acc73152251848a5e90619816b3e98dc190
NadiaBarco <48421521+NadiaBarco@users.noreply.github.com>
2024-11-15 20:49:23 -0300

    Update HeapTest.java

--- TP 2/src/main/java/aed/HeapTest.java
+++ TP 2/src/main/java/aed/HeapTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 public class HeapTest {
-    Comparator<Traslado> comparadorGanancia;// maxHeap
-    Comparator<Traslado> comparadorAntiguedad;//minHeap
+    Comparator<Traslado> ComparadorGananciaNeta;
+    Comparator<Traslado> comparadorAntiguedad;
     ArrayList<Traslado> datos;    
 
 
@@ -22,7 +22,7 @@ public class HeapTest {
         assertTrue(heap.estaVacio());
     }
 
-    /* 
+    /*
     @Test
     public void estaVacioAntiguedad(){
         Traslado[] traslados = new Traslado[]{};
@@ -36,8 +36,8 @@ public class HeapTest {
     public void insertarSoloTraslados(){
         int cantCiudades = 7;
         ArrayList<Traslado> vacio={};
-        Heap heap = new Heap(vacio, comparadorAntiguedad, true);
-        Traslado[] listaTraslados = new Traslado[] {
+        Heap heap = new Heap(vacio, comparadorAntiguedad, false);
+        ArrayList[] listaTraslados = new Traslado[] {
                     new Traslado(1, 0, 1, 100, 10),
                     new Traslado(2, 0, 1, 400, 20),
                     new Traslado(3, 3, 4, 500, 50),
@@ -53,14 +53,15 @@ public class HeapTest {
             heap.insertar(listaTraslados[i]);
         };
         assertEquals(cantCiudades, heap.tamaño());
-        assertEquals(heap.despachar(),listaTraslados[0])
+        assertEquals(heap.despachar(),listaTraslados[0]);
+        assertEquals(cantCiudades-1,heap.tamaño());
         //assertEquals(nuevo_heap.tamaño(), 10);
     }
 
     @Test
     // despacha el el indice mas antiguo en el heap de ganancias
     public void despacharIndiceAntiguedad(){
-        int cantCiudades= 18;
+        int cantCiudades= 22;
         Traslado[] traslados = new Traslado[]{
                 new Traslado(8, 2, 5, 1500, 30),
                 new Traslado(9, 0, 2, 600, 25),
@@ -69,25 +70,17 @@ public class HeapTest {
                 new Traslado(19, 5, 0, 1500, 40),
                 new Traslado(20, 6, 2, 1800, 48),
                 new Traslado(10, 3, 1, 800, 35),
-                new Traslado(11, 4, 2, 1200, 50),
-                new Traslado(12, 5, 4, 2500, 60),
-                new Traslado(13, 6, 5, 1100, 45),
-                new Traslado(14, 0, 3, 700, 20),
-                new Traslado(15, 3, 2, 400, 15),
-                new Traslado(16, 1, 4, 900, 30),
-                new Traslado(17, 2, 6, 2200, 55),
-                new Traslado(18, 4, 1, 800, 22),
-                new Traslado(19, 5, 0, 1500, 40),
-                new Traslado(20, 6, 2, 1800, 48)
-
+                new Traslado(11, 4, 2, 1200, 50)
         };
-        Heap heap = new Heap(traslados, comparadorGanancia, false);
-        assertEquals(traslados[11],despacharEnIndice())
 
+        Heap heap = new Heap(traslados, comparadorGananciaNeta, true);
+
+        //assertEquals(sistema.ciudadesConMayorGanancia(),[2])
+        //sistema.despacharMasRedituables(1);
+        //assertEquals(traslados[1],)
     }
-
     // despacha el el indice con mayor ganancia en el heap de antiguedad
-    public void despacharIndiceGanancia(){
+    //public void despacharIndiceGanancia(){
 
-    }
+    //}
 }
