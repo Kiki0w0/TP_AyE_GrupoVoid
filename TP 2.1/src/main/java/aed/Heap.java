2844966e498d4086b2c51db96e3ed0bcec541f0c
kathienar <109445081+kathienar@users.noreply.github.com>
2024-11-17 12:48:21 -0300

    algunos cambios de nombre y comentarios

--- TP 2.1/src/main/java/aed/Heap.java
+++ TP 2.1/src/main/java/aed/Heap.java
@@ -3,41 +3,37 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Heap<T> {
-    private ArrayList<Tupla<T>> datos;
+    private ArrayList<HeapElement<T>> datos;
     private Comparator<T> comparador;
 
-    public Heap(ArrayList<Tupla<T>> datos, Comparator<T> comparador){
+    // Sea N = cantidad de elementos en el heap
+
+    public Heap(ArrayList<HeapElement<T>> datos, Comparator<T> comparador){ // O(N) por algoritmo de Floyd
         this.datos = datos;
         this.comparador = comparador;
-        construirHeap();
+        heapify();
     }
 
-    public void construirHeap(){
+    public void heapify(){ // O(N)
         int n = datos.size();
         for(int i = (n - 1) / 2; i >= 0; i--){
             heapifyDown(i);
         }
     }
 
-    public int tamaño(){
+    public int tamaño(){ // O(1)
         return datos.size();
     }
 
-
-    public boolean estaVacio(){
-        return datos.size() == 0;
-    }
-
-
-    public Tupla<T> extraerMax() { // saca la tupla con el objeto de mayor prioridad y reacomoda el heap 
+    public HeapElement<T> extraerMax() { // O(log(N))
         if (datos.size() == 0) {
             return null;
         }
         if (datos.size() == 1) {
             return datos.remove(0);
         } else {
-            Tupla<T> max = datos.get(0);
-            Tupla<T> ultimo = datos.get(datos.size() - 1);
+            HeapElement<T> max = datos.get(0);
+            HeapElement<T> ultimo = datos.get(datos.size() - 1);
             ultimo.setHandle(0);
             datos.set(0, ultimo);
             datos.remove(datos.size() - 1);
@@ -46,12 +42,12 @@ public class Heap<T> {
         }
     }
 
-    public Tupla<T> devolverMax() {
+    public HeapElement<T> consultarMax() { // O(1)
         return datos.get(0);
     }
     
-    public void despacharEnIndice(int i) { // despacha en el indice que indiquemos y reacomoda el heap
-        Tupla<T> ultimo = datos.get(datos.size() - 1);
+    public void eliminar(int i) { // O(log(N)) Despacha en el handle correspondiente y reacomoda el heap
+        HeapElement<T> ultimo = datos.get(datos.size() - 1);
         ultimo.setHandle(i);
         datos.set(i, ultimo);
         datos.remove(datos.size() - 1);
@@ -59,10 +55,10 @@ public class Heap<T> {
         actualizarEnIndice(i);
     }
     
-    public void actualizarEnIndice(int i) { // reacomoda el heap cuando uno de sus valores cambia
+    public void actualizarEnIndice(int i) { // O(log(N)) Reacomoda el heap cuando alguno de sus valores Cambia
         if (i < datos.size()) {
             int padre = (i - 1) / 2;
-            if (comparador.compare(datos.get(i).getObjeto(), datos.get(padre).getObjeto()) > 0) {
+            if (comparador.compare(datos.get(i).getValor(), datos.get(padre).getValor()) > 0) {
                 heapifyUp(i);
             } else {
                 heapifyDown(i);
@@ -70,30 +66,30 @@ public class Heap<T> {
         }
     }
 
-    public void insertar(Tupla<T> objeto){ // inserta el traslado al final del heap, actualiza id y luego acomoda
+    public void insertar(HeapElement<T> objeto){ // O(log(N)) Inserta un elemento en el heap, lo acomoda y actualiza su handle correspondiente
         objeto.setHandle(datos.size() - 1);
         datos.add(objeto);
         heapifyUp(datos.size() - 1);
     }
 
-    public void heapifyUp(int i){
+    public void heapifyUp(int i){ // O(log(N))
         int padre = (i - 1) / 2;
-         if (i > 0 && comparador.compare(datos.get(i).getObjeto(), datos.get(padre).getObjeto()) > 0){
+         if (i > 0 && comparador.compare(datos.get(i).getValor(), datos.get(padre).getValor()) > 0){
             swap(i, padre);
             heapifyUp(padre);
          }
     }
 
-    public void heapifyDown(int i){
+    public void heapifyDown(int i){ // O(log(N))
         int hijoIzq = 2 * i + 1;
         int hijoDer = 2 * i + 2;
         int mayor = i;
 
-        if (hijoIzq < datos.size() && comparador.compare(datos.get(hijoIzq).getObjeto(), datos.get(mayor).getObjeto()) > 0){
+        if (hijoIzq < datos.size() && comparador.compare(datos.get(hijoIzq).getValor(), datos.get(mayor).getValor()) > 0){
             mayor = hijoIzq;
         }
 
-        if(hijoDer < datos.size() && comparador.compare(datos.get(hijoDer).getObjeto(), datos.get(mayor).getObjeto()) > 0){
+        if(hijoDer < datos.size() && comparador.compare(datos.get(hijoDer).getValor(), datos.get(mayor).getValor()) > 0){
             mayor = hijoDer;
         }
 
@@ -104,9 +100,9 @@ public class Heap<T> {
 
     }
 
-    public void swap(int i, int j){ // intercambia la posicion de traslados y actualiza los id de referencia
-        Tupla<T> tempj = datos.get(j);
-        Tupla<T> tempi = datos.get(i);
+    public void swap(int i, int j){ // O(1) Intercambia la posicion de dos elementos y actualiza sus handles correspondientes
+        HeapElement<T> tempj = datos.get(j);
+        HeapElement<T> tempi = datos.get(i);
         tempj.setHandle(i);
         tempi.setHandle(j);
         datos.set(i, tempj);
